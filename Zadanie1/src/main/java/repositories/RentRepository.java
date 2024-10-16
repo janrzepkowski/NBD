dc7139f36fd7f606b1e7c2d6534fa8d81742271b
hjatczak <hjatczak@gmail.com>
2024-10-16 23:44:43 +0200

    Implementation of constructors using entity manager for repositories.

--- Zadanie1/src/main/java/repositories/RentRepository.java
+++ Zadanie1/src/main/java/repositories/RentRepository.java
@@ -1,6 +1,8 @@
 package repositories;
 
-import jakarta.persistence.*;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.LockModeType;
+import jakarta.persistence.OptimisticLockException;
 import models.Client;
 import models.Rent;
 import models.Vehicle;
@@ -11,25 +13,24 @@ import java.util.UUID;
 
 public class RentRepository implements Repository<Rent> {
 
-    private final EntityManagerFactory emf = Persistence.createEntityManagerFactory("default");
+    private final EntityManager em;
+
+    public RentRepository(EntityManager entityManager) {
+        this.em = entityManager;
+    }
 
     @Override
     public Rent get(UUID id) {
-        try (EntityManager em = emf.createEntityManager()) {
-            return em.find(Rent.class, id);
-        }
+        return em.find(Rent.class, id);
     }
 
     @Override
     public List<Rent> getAll() {
-        try (EntityManager em = emf.createEntityManager()) {
-            return em.createQuery("FROM Rent r", Rent.class).getResultList();
-        }
+        return em.createQuery("FROM Rent r", Rent.class).getResultList();
     }
 
     @Override
     public Rent add(Rent rent) {
-        EntityManager em = emf.createEntityManager();
         try {
             em.getTransaction().begin();
             em.persist(rent);
@@ -40,14 +41,11 @@ public class RentRepository implements Repository<Rent> {
                 em.getTransaction().rollback();
             }
             throw new RuntimeException("Failed to add rent: " + rent.getRentId(), e);
-        } finally {
-            em.close();
         }
     }
 
     @Override
     public void remove(Rent rent) {
-        EntityManager em = emf.createEntityManager();
         try {
             em.getTransaction().begin();
             Rent managedRent = em.contains(rent) ? rent : em.merge(rent);
@@ -58,14 +56,11 @@ public class RentRepository implements Repository<Rent> {
                 em.getTransaction().rollback();
             }
             throw new RuntimeException("Failed to remove rent: " + rent.getRentId(), e);
-        } finally {
-            em.close();
         }
     }
 
     @Override
     public void update(Rent rent) {
-        EntityManager em = emf.createEntityManager();
         try {
             em.getTransaction().begin();
             em.merge(rent);
@@ -75,13 +70,10 @@ public class RentRepository implements Repository<Rent> {
                 em.getTransaction().rollback();
             }
             throw new RuntimeException("Failed to update rent: " + rent.getRentId(), e);
-        } finally {
-            em.close();
         }
     }
 
     public void bookVehicle(Client client, Vehicle vehicle, LocalDateTime rentStart) {
-        EntityManager em = emf.createEntityManager();
         try {
             em.getTransaction().begin();
             Vehicle managedVehicle = em.find(Vehicle.class, vehicle.getVehicleId(), LockModeType.OPTIMISTIC_FORCE_INCREMENT);
@@ -103,8 +95,6 @@ public class RentRepository implements Repository<Rent> {
                 em.getTransaction().rollback();
             }
             throw new RuntimeException("The vehicle could not be booked: " + vehicle.getVehicleId(), e);
-        } finally {
-            em.close();
         }
     }
 }
