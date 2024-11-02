15d0c9a8f01527558d28a148ba4cebbdf75e3ff1
Jan Rzepkowski 247777 <jan.rzepkowski21@gmail.com>
2024-11-02 14:26:57 +0100

    MongoDB connection management

---
+++ Zadanie2/src/main/java/repositories/AbstractMongoRepository.java
@@ -0,0 +1,58 @@
+package repositories;
+
+import com.mongodb.ConnectionString;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.MongoCredential;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoDatabase;
+import org.bson.UuidRepresentation;
+import org.bson.codecs.configuration.CodecRegistries;
+import org.bson.codecs.configuration.CodecRegistry;
+import org.bson.codecs.pojo.Conventions;
+import org.bson.codecs.pojo.PojoCodecProvider;
+
+public abstract class AbstractMongoRepository implements AutoCloseable {
+
+    private ConnectionString connectionString = new ConnectionString("mongodb://mongodb1:27017,mongodb2:27017,mongodb3:27017/?replicaSet=replica_set_single");
+    private MongoCredential credential = MongoCredential.createCredential("admin", "admin", "adminpassword".toCharArray());
+
+    private CodecRegistry pojoCodecRegistry = CodecRegistries.fromRegistries(
+            MongoClientSettings.getDefaultCodecRegistry(),
+            CodecRegistries.fromProviders(
+                    PojoCodecProvider.builder()
+                            .automatic(true)
+                            .conventions(Conventions.DEFAULT_CONVENTIONS)
+                            .build()
+            )
+    );
+
+    private MongoClient mongoClient;
+    private MongoDatabase database;
+
+    private void initDbConnection() {
+        MongoClientSettings settings = MongoClientSettings.builder()
+                .credential(credential)
+                .applyConnectionString(connectionString)
+                .uuidRepresentation(UuidRepresentation.STANDARD)
+                .codecRegistry(pojoCodecRegistry)
+                .build();
+
+        mongoClient = MongoClients.create(settings);
+        database = mongoClient.getDatabase("rental");
+    }
+
+    public MongoDatabase getDatabase() {
+        if (database == null) {
+            initDbConnection();
+        }
+        return database;
+    }
+
+    @Override
+    public void close() {
+        if (mongoClient != null) {
+            mongoClient.close();
+        }
+    }
+}
