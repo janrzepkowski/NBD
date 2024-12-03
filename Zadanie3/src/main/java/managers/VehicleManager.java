ce2ed5de997ce3064b277a476a74e707b640a8a8
Jan Rzepkowski 247777 <jan.rzepkowski21@gmail.com>
2024-12-03 20:05:55 +0100

    Vehicle manager refactoring

--- Zadanie3/src/main/java/managers/VehicleManager.java
+++ Zadanie3/src/main/java/managers/VehicleManager.java
@@ -1,15 +1,17 @@
 package managers;
 
 import models.Vehicle;
-import repositories.VehicleRepository;
+import repositories.DecoratorVehicleRepository;
 
 import java.io.Serializable;
+import java.util.List;
+import java.util.UUID;
 
 public class VehicleManager implements Serializable {
 
-    private final VehicleRepository vehicleRepository;
+    private final DecoratorVehicleRepository vehicleRepository;
 
-    public VehicleManager(VehicleRepository vehicleRepository) {
+    public VehicleManager(DecoratorVehicleRepository vehicleRepository) {
         if (vehicleRepository == null) {
             throw new IllegalArgumentException("vehicleRepository cannot be null");
         } else {
@@ -17,17 +19,48 @@ public class VehicleManager implements Serializable {
         }
     }
 
+    private boolean vehicleExists(UUID vehicleId) {
+        List<Vehicle> vehicles = vehicleRepository.readAll();
+        for (Vehicle vehicle : vehicles) {
+            if (vehicle.getVehicleId().equals(vehicleId)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public Vehicle getVehicle(UUID vehicleId) {
+        return vehicleRepository.read(vehicleId);
+    }
+
+    public List<Vehicle> getAllVehicles() {
+        return vehicleRepository.readAll();
+    }
+
     public void registerVehicle(Vehicle vehicle) {
-        if (vehicleRepository.read(vehicle.getVehicleId()) != null) {
+        if (!vehicleExists(vehicle.getVehicleId())) {
+            vehicleRepository.create(vehicle);
+        } else {
             throw new IllegalArgumentException("Vehicle with the same ID already exists.");
         }
-        vehicleRepository.create(vehicle);
     }
 
     public void unregisterVehicle(Vehicle vehicle) {
         if (vehicle != null) {
             vehicle.setArchived(true);
             vehicleRepository.update(vehicle);
+        }
+    }
+
+    public void updateVehicleInformation(Vehicle vehicle) {
+        if (vehicleExists(vehicle.getVehicleId())) {
+            vehicleRepository.update(vehicle);
+        }
+    }
+
+    public void deleteVehicle(UUID vehicleId) {
+        if (vehicleExists(vehicleId)) {
+            vehicleRepository.delete(vehicleRepository.read(vehicleId));
         }
     }
 }
